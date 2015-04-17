80d49f871f6f02feface2951644c36d598c48f9c
Graham Park <grahamjpark@users.noreply.github.com>
2015-04-17 19:47:41 -0400

    Create jacksquatbot.js

---
+++ jacksquatbot.js
@@ -0,0 +1,62 @@
+var MYUSER = 'tim_plays_blackjack';
+var MYPASS = 'letsgotim';
+
+var request = require('request');
+
+var ENDPOINT = 'http://hackjack.purduehackers.com/';
+
+// Create Table
+
+request.post({url: ENDPOINT + 'tables/TABLENAMEHERE',
+             form: {username: MYUSER, password: MYPASS, command: 'create'}},
+             function(err,httpResponse,body) {
+  console.log(body);
+});
+
+// Start Table
+
+request.post({url: ENDPOINT + 'tables/TABLENAMEHERE',
+             form: {username: MYUSER, password: MYPASS, command: 'start'}},
+             function(err,httpResponse,body) {
+  console.log(body);
+});
+
+// Join Table
+
+request.post({url: ENDPOINT + 'tables/TABLENAMEHERE',
+             form: {username: MYUSER, password: MYPASS, command: 'join'}},
+             function(err,httpResponse,body) {
+  console.log(body);
+});
+
+// Leave Table
+
+request.post({url: ENDPOINT + 'tables/TABLENAMEHERE',
+             form: {username: MYUSER, password: MYPASS, command: 'leave'}},
+             function(err,httpResponse,body) {
+  console.log(body);
+});
+
+// Place Bet
+
+request.post({url: ENDPOINT + 'tables/TABLENAMEHERE',
+             form: {username: MYUSER, password: MYPASS, command: 'bet', bet_amt: 10}},
+             function(err,httpResponse,body) {
+  console.log(body);
+});
+
+// Hit
+
+request.post({url: ENDPOINT + 'tables/TABLENAMEHERE',
+             form: {username: MYUSER, password: MYPASS, command: 'hit'}},
+             function(err,httpResponse,body) {
+  console.log(body);
+});
+
+// Stay
+
+request.post({url: ENDPOINT + 'tables/TABLENAMEHERE',
+             form: {username: MYUSER, password: MYPASS, command: 'stay'}},
+             function(err,httpResponse,body) {
+  console.log(body);
+});
