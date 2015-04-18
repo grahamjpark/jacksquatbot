c99a1333365a76e5125012a0da538fd2fa6dd520
Tim Vincent <vincent3@purdue.edu>
2015-04-17 21:41:27 -0400

    Added logic

--- jacksquatbot.js
+++ jacksquatbot.js
@@ -4,7 +4,7 @@ var MYPASS = 'letsgotim';
 var request = require('request');
 
 var ENDPOINT = 'http://hackjack.purduehackers.com/';
-var TABLE = '3dogs';
+var TABLE = '4dogs';
 
 var PLAYER = 0;
 var DEALER = 1;
@@ -36,27 +36,109 @@ request.post({url: ENDPOINT + 'tables/' + TABLE,
 // Place Bet
 //if (num_aces(my_hand) == 0) {
 if(numCard("A", PLAYER) == 0){
-    console.log("HE LLLLLLLLLLLLLLLLLLOOOOOOOOO");
     if(get_amount(DEALER) <= 8){ 
-      hit();
+      hit();//Bet?
   }
   else if(get_amount(PLAYER) == 9){
-
+      if(get_amount(DEALER) >= 3 && get_amount(get_amount) <= 6){
+        hit();//Bet?
+      }
+      else {
+        hit();
+      }
   }
-}
+  else if(get_amount(PLAYER) == 10) {
+    if(get_amount(DEALER) >= 2 && get_amount(DEALER) <= 9) {
+      hit();//Bet
+    }
+    else {
+      hit();
+    }
+  }
+  else if(get_amount(PLAYER) == 11) {
+    if(numCard("A", DEALER) >= 1) {
+      hit();
+    }
+    else if(get_amount(DEALER) >= 2 && get_amount(DEALER) <= 10){
+      hit(); //bet
+    }
+  }
+  else if(get_amount(PLAYER) == 12) {
+    if(get_amount(DEALER)>= 2 && get_amount(DEALER) <= 6){
+      stay();
+    }
+    else {
+      hit();
+    }
+  }
+  else if(get_amount(PLAYER) >= 13 && get_amount(PLAYER) <= 16){
+    if(get_amount(dealer_hand) >= 2 && get_amount(dealer_hand) <= 6) {
+       stay();
+    }
+    else {
+      hit();
+    }
+  }
+  else {
+    stay(); 
+  }
+
+} //End hard hand
+else {
+  if(numCard("A", PLAYER) == 1 && numCard("2", PLAYER) || numCard("A", PLAYER) == 1 && numCard("3", PLAYER)) {
+    if(get_amount(DEALER) >= 5 && get_amount(DEALER) <= 6){
+      hit();//Bet
+    }
+    else {
+      hit();
+    }
+  }
+  else if(numCard("A", PLAYER) == 1 && numCard("4", PLAYER) || numCard("A", PLAYER) == 1 && numCard("5", PLAYER)) {
+    if(get_amount(DEALER) >= 4 && get_amount(DEALER) <= 6){
+      hit();//Bet
+    }
+    else {
+      hit();
+    } 
+  }
+  else if(numCard("A", PLAYER) == 1 && numCard("6", PLAYER)) {
+    if(get_amount(DEALER) >= 3 && get_amount(DEALER) <= 6){
+      hit();//Bet
+    }
+    else {
+      hit();
+    } 
+  }
+  else if(numCard("A", PLAYER) == 1 && numCard("7", PLAYER)) {
+    if(get_amount(DEALER) == 2 || get_amount(DEALER) == 7 || get_amount(DEALER) == 8){
+      stay();
+    }
+    else if(get_amount(DEALER) >= 3 && get_amount(DEALER) <= 6) {
+      hit();
+      bet();
+    } 
+    else {
+      bet();
+    }
+  }
+  else if(numCard("A", PLAYER) == 1 && numCard('8', PLAYER)) {
+    stay();
+  }
+  else if(numCard("A", PLAYER) == 1 && numCard('9', PLAYER)) {
+    stay();
+  }
+  else {
+    stay();
+  }
+} //End soft hand
 // Hit
 //bet(10);
 
-
 // Stay
 
-request.post({url: ENDPOINT + 'tables/' + TABLE,
-             form: {username: MYUSER, password: MYPASS, command: 'stay'}},
-             function(err,httpResponse,body) {
-  console.log(body);
-});
 
-leave();
+
+//leave();
 
 function leave() {
   // Leave Table
@@ -99,4 +181,12 @@ function hit(){
              function(err,httpResponse,body) {
     console.log(body);
   });
+}
+
+function stay() {
+  request.post({url: ENDPOINT + 'tables/' + TABLE,
+             form: {username: MYUSER, password: MYPASS, command: 'stay'}},
+             function(err,httpResponse,body) {
+  console.log(body);
+});
 }
