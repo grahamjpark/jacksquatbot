bdf16a6454412b6dffa77e8ba8fa0ed000aec391
Tim Vincent <vincent3@purdue.edu>
2015-04-17 20:49:08 -0400

    Latest

--- jacksquatbot.js
+++ jacksquatbot.js
@@ -4,14 +4,16 @@ var MYPASS = 'letsgotim';
 var request = require('request');
 
 var ENDPOINT = 'http://hackjack.purduehackers.com/';
-var TABLE = 'dogs';
-// Create Table
+var TABLE = '3dogs';
 
-request.post({url: ENDPOINT + 'tables/' + TABLE,
+var PLAYER = 0;
+var DEALER = 1;
+// Create Table
+  request.post({url: ENDPOINT + 'tables/' + TABLE,
              form: {username: MYUSER, password: MYPASS, command: 'create'}},
              function(err,httpResponse,body) {
-  console.log("Created: " + body);
-});
+    console.log("Created: " + body);
+  });
 
 // Start Table
 
@@ -30,21 +32,21 @@ request.post({url: ENDPOINT + 'tables/' + TABLE,
 });
 
 
+
 // Place Bet
+//if (num_aces(my_hand) == 0) {
+if(numCard("A", PLAYER) == 0){
+    console.log("HE LLLLLLLLLLLLLLLLLLOOOOOOOOO");
+    if(get_amount(DEALER) <= 8){ 
+      hit();
+  }
+  else if(get_amount(PLAYER) == 9){
 
-request.post({url: ENDPOINT + 'tables/' + TABLE,
-             form: {username: MYUSER, password: MYPASS, command: 'bet', bet_amt: 10}},
-             function(err,httpResponse,body) {
-  console.log(body);
-});
-
+  }
+}
 // Hit
+//bet(10);
 
-request.post({url: ENDPOINT + 'tables/' + TABLE,
-             form: {username: MYUSER, password: MYPASS, command: 'hit'}},
-             function(err,httpResponse,body) {
-  console.log(body);
-});
 
 // Stay
 
@@ -54,12 +56,47 @@ request.post({url: ENDPOINT + 'tables/' + TABLE,
   console.log(body);
 });
 
+leave();
 
 function leave() {
   // Leave Table
   request.post({url: ENDPOINT + 'tables/' + TABLE,
               form: {username: MYUSER, password: MYPASS, command: 'leave'}},
               function(err,httpResponse,body) {
+    console.log(body);
+  });
+}
+
+function numCard(card, hand) {
+  var count = 0;
+  //if hand 0 then player
+    //if playerhand.contains(card)
+        //count++;
+  //else {
+
+    //}
+   return count; 
+}
+
+function get_amount(hand){
+  var count = 0;
+
+  return count;
+}
+
+function bet(){
+  request.post({url: ENDPOINT + 'tables/' + TABLE,
+             form: {username: MYUSER, password: MYPASS, command: 'bet', bet_amt: 10}},
+             function(err,httpResponse,body) {
+  console.log(body);
+  });
+}
+
+
+function hit(){
+    request.post({url: ENDPOINT + 'tables/' + TABLE,
+             form: {username: MYUSER, password: MYPASS, command: 'hit'}},
+             function(err,httpResponse,body) {
     console.log(body);
   });
 }
